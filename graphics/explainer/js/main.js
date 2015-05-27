265afbba53773f1bbbcdcdaa9393c64fc70852e4
Gabriel Florit <gabrielflorit@gmail.com>
2015-05-27 02:27:05 -0400

    trying to make transitions work

--- graphics/explainer/js/main.js
+++ graphics/explainer/js/main.js
@@ -47,7 +47,7 @@ $(`${masterSelector} .buttons button`).click(function() {
 // Next: time to make the chart. First we'll create a blank chart with margins.
 // NOTE: this assumes the viewport will never change. Obviously this assumption
 // is wrong. We'll revisit this at a later date.
-var margin = {top: 0, right: 0, bottom: 30, left: 30};
+var margin = {top: 0, right: 0, bottom: 0, left: 0};
 var svgWidth = $(chartSelector).outerWidth();
 var svgHeight = $(chartSelector).outerHeight();
 var width = svgWidth - margin.left - margin.right;
@@ -67,10 +67,11 @@ var g = svg.append('g')
 
 
 // Next: initialize charts with their various datasets.
-tripsDaily.init({data: require('../../../data/output/trips-daily.csv')});
+tripsDaily.init({g, data: require('../../../data/output/trips-daily.csv')});
 
 // This function takes care of calling the right chart.
 function drawChart(_stepIndex) {
+
 	var dimensions = {
 		width,
 		height
--- graphics/explainer/js/tripsDaily.js
+++ graphics/explainer/js/tripsDaily.js
@@ -9,6 +9,7 @@ var parseDate = d3.time.format('%Y-%m-%d').parse;
 // Store chart data here.
 var data = null;
 
+// Declare scale variables.
 var x;
 var y;
 
@@ -24,12 +25,12 @@ function setScales(opts) {
 	y = d3.scale.linear()
 		.range([opts.dimensions.height, 0]);
 
-	switch(opts.scene) {
-		case 'intro':
-			x.domain(d3.extent(_.take(data, 1), d => d.date));
-			y.domain([0, d3.max(_.take(data, 1), d => d.trips)]);
-			break;
-	}
+	//switch(opts.scene) {
+		//case 'intro':
+	x.domain(d3.extent(data, d => d.date));
+	y.domain([0, d3.max(data, d => d.trips)]);
+			//break;
+	//}
 }
 
 function prepareToDraw(opts) {
@@ -38,11 +39,31 @@ function prepareToDraw(opts) {
 	setScales(opts);
 
 	// Draw!
-	draw(opts);
+	//draw(opts);
+
+
+	var shuffledTrips = _.chain(data)
+		.shuffle()
+		.take(5)
+		.sortBy('date')
+		.value();
+
+	update(shuffledTrips, opts);
+	setInterval(function() {
+		var shuffledTrips = _.chain(data)
+			.shuffle()
+			.take(5)
+			.sortBy('date')
+			.value();
+		
+		update(shuffledTrips, opts);
+	}, 4000);
 }
 
 function draw(opts) {
 
+	console.log(opts);
+	debugger;
 	// This is the drawing function. This is where we draw.
 	// If we're on the first scene, 'intro',
 	// set the x-axis to ... the first 10.
@@ -53,38 +74,65 @@ function draw(opts) {
 
 	switch(opts.scene) {
 		case 'intro':
-			sceneData = _.take(data, 1);
+			sceneData = _.take(data, 3);
+			sceneData[0].trips = 100;
+			break;
+		case 'firstDay':
+			sceneData = _.take(data, 3);
+			sceneData[0].trips = data[0].trips;
 			break;
 	}
+	console.log(data);
+	console.log(sceneData);
 
-	var rect = opts.g.append('g')
+	debugger;
+	var rect = opts.g.select('g.trips-daily')
 		.selectAll('rect')
-		.data(sceneData, d => d.date);
+		.data(sceneData, d => d);
 
 	// UPDATE
 	// Update old elements as needed.
+	rect
+		.transition().duration(5000)
+		.attr({
+			y: d => y(d.trips),
+			height: d => opts.dimensions.height - y(d.trips)
+		});
 
 	// ENTER
 	// Create new elements as needed.
 	switch(opts.scene) {
 		case 'intro':
-			sceneData = _.take(data, 1);
 			rect.enter().append('rect')
 				.attr({
-					x: d => x(d.date),
+					x: x.range()[1]/3,
+					width: 50,
 					y: d => y(d.trips),
-					width: x.range()[1] / sceneData.length,
 					height: d => opts.dimensions.height - y(d.trips)
+				});
+			break;
+		case 'firstDay':
+			rect.enter().append('rect')
+				.attr({
+					x: x.range()[1]/3,
+					width: 50
 				});
 			break;
 	}
 
-	// EXIT
-	// Remove old elements as needed.
-	rect.exit().remove();
+	//rect.attr({
+		//y: d => y(d.trips),
+		//height: d => opts.dimensions.height - y(d.trips)
+	//});
+	//// EXIT
+	//// Remove old elements as needed.
+	//rect.exit().remove();
 }
 
 function init(opts) {
+	opts.g.append('g')
+		.attr('class', 'trips-daily');
+
 	data = opts.data.map(function(datum) {
 		return {
 			date: parseDate(datum.date),
@@ -93,7 +141,128 @@ function init(opts) {
 	});
 }
 
+function demo_draw() {
+}
+
 module.exports = {
 	draw: prepareToDraw,
 	init: init
 };
+
+function update(_data, opts) {
+	var g = d3.select('g.trips-daily');
+
+	// DATA JOIN
+	// Join new data with old elements, if any.
+	var rect = g.selectAll('rect')
+		.data(_data, d => d.date);
+
+	// UPDATE
+	// Update old elements as needed.
+	rect.attr('class', 'update')
+		.transition()
+		.duration(1500)
+		.attr({
+			x: d => x(d.date)
+		});
+
+	// ENTER
+	// Create new elements as needed.
+	rect.enter().append('rect')
+		.attr({
+			'class': 'enter',
+			x: d => x(d.date),
+			y: 0,
+			width: x.range()[1] / data.length,
+			height: d => opts.dimensions.height - y(d.trips)
+		})
+		.transition()
+		.duration(1500)
+		.attr({
+			y: d => y(d.trips)
+		});
+			
+	// EXIT
+	// Remove old elements as needed.
+	rect.exit()
+		.attr('class', 'exit')
+		.transition()
+		.duration(1500)
+		.attr({
+			y: 300
+		})
+		.style('fill-opacity', 1e-6)
+		.remove();
+
+	//text.exit()
+		//.attr("class", "exit")
+		//.transition()
+		//.duration(1500)
+		//.attr("y", 60)
+		//.style("fill-opacity", 1e-6)
+		//.remove();
+}
+function updateAlphabet(data, svg) {
+
+var svg = d3.select('g.trips-daily');
+console.log(data);
+  // DATA JOIN
+  // Join new data with old elements, if any.
+  var text = svg.selectAll("text")
+      .data(data, function(d) { return d; });
+
+  // UPDATE
+  // Update old elements as needed.
+  text.attr("class", "updatet")
+    .transition()
+      .duration(1500)
+      .attr("x", function(d, i) { return i * 32; });
+
+  // ENTER
+  // Create new elements as needed.
+  text.enter().append("text")
+      .attr("class", "enter")
+      .attr("dy", "2em")
+      .attr("y", -60)
+      .attr("x", function(d, i) { return i * 32; })
+      .style("fill-opacity", 1e-6)
+      .text(function(d) { return d; })
+    .transition()
+      .duration(1500)
+      .attr("y", 0)
+      .style("fill-opacity", 1);
+
+  // EXIT
+  // Remove old elements as needed.
+  text.exit()
+      .attr("class", "exit")
+    .transition()
+      .duration(1500)
+      .attr("y", 60)
+      .style("fill-opacity", 1e-6)
+      .remove();
+}
+
+// The initial display.
+updateAlphabet(alphabet);
+
+// Grab a random sample of letters from the alphabet, in alphabetical order.
+setInterval(function() {
+  updateAlphabet(shuffle(alphabet)
+      .slice(0, Math.floor(Math.random() * 26))
+      .sort()
+	);
+}, 4000);
+
+// Shuffles the input array.
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m], array[m] = array[i], array[i] = t;
+  }
+  return array;
+}
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
