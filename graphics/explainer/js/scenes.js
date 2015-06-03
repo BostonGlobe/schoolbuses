22ed42996152aeae961dbe373e38eefaae6b8a95
Gabriel Florit <gabrielflorit@gmail.com>
2015-06-03 12:30:50 -0400

    start working on detailed slides

--- graphics/explainer/js/scenes.js
+++ graphics/explainer/js/scenes.js
@@ -7,7 +7,8 @@ var width;
 var height;
 var scene;
 var chart;
-var rects;
+var dailyTripsRectangles;
+var firstDayRectangles;
 var dataKeys;
 
 var axes = {
@@ -16,12 +17,6 @@ var axes = {
 };
 
 var singleBarWidth = 50;
-
-var scales = {
-	x: d3.time.scale(),
-	y: d3.scale.linear(),
-	color: d3.scale.ordinal()
-}
 
 function log(s) {
 	console.log(JSON.stringify(s, null, 4));
@@ -46,7 +41,12 @@ module.exports = {
 			.uniq()
 			.value();
 
-		// Setup scales
+		var scales = {
+			x: d3.time.scale(),
+			y: d3.scale.linear(),
+			color: d3.scale.ordinal()
+		};
+
 		scales.x.range([0, width]).domain(d3.extent(_.take(datasets.tripsPerDay, 4), d => d.date));
 		scales.y.range([height, 0]).domain([0, 0]);
 		scales.color.range([dark, dark]).domain(dataKeys);
@@ -62,9 +62,13 @@ module.exports = {
 			.orient('left')
 			.tickSize(-width);
 
-		// DATA JOIN
-		rects = chart.selectAll('rect')
+		// DATA JOINS
+		dailyTripsRectangles = chart.selectAll('rect')
 			.data(datasets.tripsPerDay, d => `${d.name}${d.date}`);
+
+		// // TODO: don't use magic string here
+		// firstDayRectangles = chart.selectAll('rect.firstDay')
+		// 	.data(_.take(datasets.allLateTrips, 648), d => `${d.index}${d.date}`);
 
 		var attributes = {
 			x: d => scales.x(d.date),
@@ -74,7 +78,7 @@ module.exports = {
 		};
 
 		// UPDATE
-		rects.transition()
+		dailyTripsRectangles.transition()
 			.duration(duration)
 			.attr(attributes)
 			.style({
@@ -82,7 +86,7 @@ module.exports = {
 			});
 
 		// ENTER
-		rects.enter().append('rect')
+		dailyTripsRectangles.enter().append('rect')
 			.attr('class', 'enter')
 			.attr(attributes)
 			.style({
@@ -129,19 +133,26 @@ module.exports = {
 		$('.x-axis-label span').text('Bus trips');
 		$('.x-axis-label').addClass('fadedIn');
 
+		var scales = {
+			x: d3.time.scale(),
+			y: d3.scale.linear(),
+			color: d3.scale.ordinal()
+		};
+
 		// Setup scales
-		scales.x.domain(d3.extent(_.take(datasets.tripsPerDay, 4), d => d.date));
-		scales.y.domain([0, d3.max(_.take(datasets.tripsPerDay, 2), d => d.y1)]);
-		scales.color.range([dark, dark]);
+		scales.x.range([0, width]).domain(d3.extent(_.take(datasets.tripsPerDay, 4), d => d.date));
+		scales.y.range([height, 0]).domain([0, d3.max(_.take(datasets.tripsPerDay, 2), d => d.y1)]);
+		scales.color.range([dark, dark]).domain(dataKeys);
 
 		// Setup axes
 		axes.x.scale(scales.x)
-			.ticks(d3.time.days, 1)
-			.tickFormat(d3.time.format('%A, %B %e'));
-		axes.y.tickValues([scales.y.domain()[1]]);
+			.ticks(d3.time.months, 3)
+			.tickFormat(null);
+		axes.y.scale(scales.y)
+			.tickValues([scales.y.domain()[1]]);
 
 		// UPDATE bars
-		rects.transition()
+		dailyTripsRectangles.transition()
 			.duration(duration)
 			.attr({
 				x: d => scales.x(d.date),
@@ -158,19 +169,9 @@ module.exports = {
 			.transition()
 			.duration(duration)
 			.call(axes.x);
-		// Fade it in
+		// Fade it out
 		xAxisSelection.attr({
-				opacity: 1
-			});
-		// Hide domain
-		xAxisSelection.select('path.domain')
-			.attr({
 				opacity: 0
-			});
-		// Shift tick texts to bar center
-		xAxisSelection.selectAll('.tick text')
-			.attr({
-				transform: `translate(${singleBarWidth/2}, 0)`
 			});
 
 		// Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y 
@@ -190,19 +191,26 @@ module.exports = {
 		$('.x-axis-label span').text('Bus trips');
 		$('.x-axis-label').addClass('fadedIn');
 
+		var scales = {
+			x: d3.time.scale(),
+			y: d3.scale.linear(),
+			color: d3.scale.ordinal()
+		};
+
 		// Setup scales
-		scales.x.domain(d3.extent(datasets.tripsPerDay, d => d.date));
-		scales.y.domain([0, d3.max(datasets.tripsPerDay, d => d.y1)]);
-		scales.color.range([dark, dark]);
+		scales.x.range([0, width]).domain(d3.extent(datasets.tripsPerDay, d => d.date));
+		scales.y.range([height, 0]).domain([0, d3.max(datasets.tripsPerDay, d => d.y1)]);
+		scales.color.range([dark, dark]).domain(dataKeys);
 
 		// Setup axes
 		axes.x.scale(scales.x)
 			.ticks(d3.time.months, 3)
 			.tickFormat(null);
-		axes.y.tickValues([0, 500, 1000, scales.y.domain()[1]]);
+		axes.y.scale(scales.y)
+			.tickValues([0, 500, 1000, scales.y.domain()[1]]);
 
 		// UPDATE
-		rects.transition()
+		dailyTripsRectangles.transition()
 			.duration(duration)
 			.attr('class', 'update')
 			.attr({
@@ -247,19 +255,26 @@ module.exports = {
 		$('.x-axis-label span').text('Bus trips');
 		$('.x-axis-label').addClass('fadedIn');
 
+		var scales = {
+			x: d3.time.scale(),
+			y: d3.scale.linear(),
+			color: d3.scale.ordinal()
+		};
+
 		// Setup scales
-		scales.x.domain(d3.extent(datasets.tripsPerDay, d => d.date));
-		scales.y.domain([0, d3.max(datasets.tripsPerDay, d => d.y1)]);
-		scales.color.range([dark, red]);
+		scales.x.range([0, width]).domain(d3.extent(datasets.tripsPerDay, d => d.date));
+		scales.y.range([height, 0]).domain([0, d3.max(datasets.tripsPerDay, d => d.y1)]);
+		scales.color.range([dark, red]).domain(dataKeys);
 
 		// Setup axes
 		axes.x.scale(scales.x)
 			.ticks(d3.time.months, 3)
 			.tickFormat(null);
-		axes.y.tickValues([0, 500, 1000, scales.y.domain()[1]]);
+		axes.y.scale(scales.y)
+			.tickValues([0, 500, 1000, scales.y.domain()[1]]);
 
 		// UPDATE
-		rects.transition()
+		dailyTripsRectangles.transition()
 			.duration(duration)
 			.attr('class', 'update')
 			.attr({
@@ -312,19 +327,26 @@ module.exports = {
 			.last()
 			.value();
 
+		var scales = {
+			x: d3.time.scale(),
+			y: d3.scale.linear(),
+			color: d3.scale.ordinal()
+		};
+
 		// Setup scales
-		scales.x.domain(d3.extent(datasets.tripsPerDay, d => d.date));
-		scales.y.domain([0, maxLateTrips]);
-		scales.color.range([dark, red]);
+		scales.x.range([0, width]).domain(d3.extent(datasets.tripsPerDay, d => d.date));
+		scales.y.range([height, 0]).domain([0, maxLateTrips]);
+		scales.color.range([dark, red]).domain(dataKeys);
 
 		// Setup axes
 		axes.x.scale(scales.x)
 			.ticks(d3.time.months, 3)
 			.tickFormat(null);
-		axes.y.tickValues([0, 200, 400, scales.y.domain()[1]]);
+		axes.y.scale(scales.y)
+			.tickValues([0, 200, 400, scales.y.domain()[1]]);
 
 		// UPDATE
-		rects.transition()
+		dailyTripsRectangles.transition()
 			.duration(duration)
 			.attr('class', 'update')
 			.attr({
@@ -366,6 +388,170 @@ module.exports = {
 				opacity: 1
 			});
 	},
+
+	'late-trips-first-day': function(duration = 1500) {
+
+		// We need to arrange all these dots into a square.
+		// But what shape? We can determine that by figuring out
+		// what shape we'll be drawing to.
+
+		// Get the first day of late trips (for now). TODO.
+		var data = _.take(datasets.allLateTrips, 648);
+
+		// Group trips by how late they were.
+		var buckets = _.chain(data)
+			.countBy('lateMinutes')
+			.map(function(v, i) {
+				return {
+					length: v,
+					lateMinutes: i
+				}
+			})
+			// .sortBy('length')
+			.value();
+
+		// var maxBucket = _.last(buckets).length;
+
+		// Setup scales
+		var scales = {
+			x: d3.scale.linear(),
+			y: d3.scale.linear()
+			// ,
+			// color: d3.scale.ordinal()
+		};
+
+		scales.x.domain([0, d3.max(buckets, d => d.length)]);
+		scales.y.domain([0, d3.max(data, d => d.lateMinutes)]);
+
+		// Now we have the shape.
+		// Next, we'll lay out all the squares on a column that's
+		// 96 tall. We'll keep filling the 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// debugger;
+		// scales.color.range([dark, dark]).domain(dataKeys);
+
+		// ENTER
+		firstDayRectangles.enter().append('rect')
+			.attr('class', 'enter');
+
+
+
+
+
+
+		// UPDATE
+		// firstDayRectangles.transition()
+		// 	.duration(duration)
+		// 	.attr(attributes)
+		// 	.style({
+		// 		fill: d => scales.color(d.name)
+		// 	});
+
+
+
+
+		// svg = d3.select('svg.scenes');
+		// width = +svg.attr('_innerWidth');
+		// height = +svg.attr('_innerHeight');
+		// scene = svg.select('g.scene');
+		// chart = scene.select('g.chart');
+
+		// // Get data keys
+		// dataKeys = _.chain(datasets.tripsPerDay)
+		// 	.pluck('name')
+		// 	.uniq()
+		// 	.value();
+
+		// // Setup scales
+		// scales.x.range([0, width]).domain(d3.extent(_.take(datasets.tripsPerDay, 4), d => d.date));
+		// scales.y.range([height, 0]).domain([0, 0]);
+		// scales.color.range([dark, dark]).domain(dataKeys);
+
+		// // Setup axes
+		// axes.x.scale(scales.x)
+		// 	.orient('bottom')
+		// 	.ticks(d3.time.days, 1)
+		// 	.tickFormat(d3.time.format('%A, %B %e'))
+		// 	.tickSize(0)
+		// 	.tickPadding(6);
+		// axes.y.scale(scales.y)
+		// 	.orient('left')
+		// 	.tickSize(-width);
+
+		// // DATA JOIN
+		// dailyTripsRectangles = chart.selectAll('rect')
+		// 	.data(datasets.tripsPerDay, d => `${d.name}${d.date}`);
+
+		// var attributes = {
+		// 	x: d => scales.x(d.date),
+		// 	width: singleBarWidth,
+		// 	y: d => scales.y(d.y1),
+		// 	height: d => scales.y(d.y0) - scales.y(d.y1)
+		// };
+
+		// // UPDATE
+		// dailyTripsRectangles.transition()
+		// 	.duration(duration)
+		// 	.attr(attributes)
+		// 	.style({
+		// 		fill: d => scales.color(d.name)
+		// 	});
+
+		// // ENTER
+		// dailyTripsRectangles.enter().append('rect')
+		// 	.attr('class', 'enter')
+		// 	.attr(attributes)
+		// 	.style({
+		// 		fill: d => scales.color(d.name)
+		// 	});
+
+		// // X X X X X X X X X X X X X X X X X X X X X X 
+		// var xAxisSelection = scene.select('g.x.axis')
+		// 	.transition()
+		// 	.duration(duration)
+		// 	.call(axes.x);
+		// // Fade it out
+		// xAxisSelection.attr({
+		// 		opacity: 0
+		// 	});
+		// // Hide domain
+		// xAxisSelection.select('path.domain')
+		// 	.attr({
+		// 		opacity: 0
+		// 	});
+		// // Shift tick texts to bar center
+		// xAxisSelection.selectAll('.tick text')
+		// 	.attr({
+		// 		transform: `translate(${singleBarWidth/2}, 0)`
+		// 	});
+
+		// // Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y 
+		// var yAxisSelection = scene.select('g.y.axis')
+		// 	.transition()
+		// 	.duration(duration)
+		// 	.call(axes.y);
+		// // Fade it out
+		// yAxisSelection.attr({
+		// 		opacity: 0
+		// 	});
+
+		// // Hide x-axis label
+		// $('.x-axis-label').removeClass('fadedIn');		
+	}
 
 };
 
@@ -451,11 +637,11 @@ module.exports = {
 	// 		.domain(dataKeys);
 
 	// 	// DATA JOIN
-	// 	rects = scene.selectAll('rect')
+	// 	dailyTripsRectangles = scene.selectAll('rect')
 	// 		.data(datasets.tripsPerDay, d => `${d.name}${d.date}`);
 
 	// 	// UPDATE
-	// 	rects.transition()
+	// 	dailyTripsRectangles.transition()
 	// 		.duration(duration)
 	// 		.attr({
 	// 			x: d => scales.x(d.date),
@@ -467,7 +653,7 @@ module.exports = {
 	// 		});
 
 	// 	// ENTER
-	// 	rects.enter().append('rect')
+	// 	dailyTripsRectangles.enter().append('rect')
 	// 		.attr('class', 'enter')
 	// 		.attr({
 	// 			x: d => scales.x(d.date),
